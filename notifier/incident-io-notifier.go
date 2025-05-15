b03431fd1c0710dbd8050bcfd43f711687221207
Tom Wardill <thomaswa@payoneer.com>
2025-05-15 15:57:02 +0100

    Iterate messages to send rather than batch

--- notifier/incident-io-notifier.go
+++ notifier/incident-io-notifier.go
@@ -31,59 +31,67 @@ func (notifier *IncidentIONotifier) Copy() Notifier {
 
 // Notify sends messages to the endpoint notifier
 func (notifier *IncidentIONotifier) Notify(messages Messages) bool {
-	overallStatus, pass, warn, fail := messages.Summary()
-	t := TemplateData{
-		ClusterName:  notifier.ClusterName,
-		SystemStatus: overallStatus,
-		FailCount:    fail,
-		WarnCount:    warn,
-		PassCount:    pass,
-		Nodes:        mapByNodes(messages),
-	}
-	values := map[string]any{}
 
-	for key, val := range notifier.Payload {
-		data, err := renderTemplate(t, "", val)
+	for _, message := range messages {
+		// Messages is a slice of Message, but we need to send a single message
+		// at a time and all of our rendering expects a slice
+		// So make a single element slice and use that
+		single_messages := make(Messages, 0)
+		single_messages = append(single_messages, message)
+
+		overallStatus, pass, warn, fail := messages.Summary()
+		t := TemplateData{
+			ClusterName:  notifier.ClusterName,
+			SystemStatus: overallStatus,
+			FailCount:    fail,
+			WarnCount:    warn,
+			PassCount:    pass,
+			Nodes:        mapByNodes(single_messages),
+		}
+		values := map[string]any{}
+
+		for key, val := range notifier.Payload {
+			data, err := renderTemplate(t, "", val)
+			if err != nil {
+				log.Println("Error rendering template: ", err)
+				return false
+			}
+			values[key] = string(data)
+		}
+
+		metadataValues := map[string]string{}
+		for key, val := range notifier.Metadata {
+			data, err := renderTemplate(t, "", val)
+			if err != nil {
+				log.Println("Error rendering template: ", err)
+				return false
+			}
+			metadataValues[key] = string(data)
+		}
+
+		values["metadata"] = metadataValues
+
+		requestBody, err := json.Marshal(values)
 		if err != nil {
-			log.Println("Error rendering template: ", err)
+			log.Println("Unable to encode POST data")
 			return false
 		}
-		values[key] = string(data)
-	}
 
-	metadataValues := map[string]string{}
-	for key, val := range notifier.Metadata {
-		data, err := renderTemplate(t, "", val)
-		if err != nil {
-			log.Println("Error rendering template: ", err)
-			return false
-		}
-		metadataValues[key] = string(data)
-	}
-
-	values["metadata"] = metadataValues
-
-	requestBody, err := json.Marshal(values)
-	if err != nil {
-		log.Println("Unable to encode POST data")
-		return false
-	}
-
-	endpoint := fmt.Sprintf("%s%s", notifier.BaseURL, notifier.Endpoint)
-	if res, err := http.Post(endpoint, "application/json", bytes.NewBuffer(requestBody)); err != nil {
-		log.Println("Unable to send data to incident.io endpoint:", err)
-		return false
-	} else {
-		defer res.Body.Close()
-		statusCode := res.StatusCode
-		if statusCode != 202 {
-			body, _ := ioutil.ReadAll(res.Body)
-			log.Println("Unable to notify incident.io endpoint:", string(body))
+		endpoint := fmt.Sprintf("%s%s", notifier.BaseURL, notifier.Endpoint)
+		if res, err := http.Post(endpoint, "application/json", bytes.NewBuffer(requestBody)); err != nil {
+			log.Println("Unable to send data to incident.io endpoint:", err)
 			return false
 		} else {
-			log.Println("Notification sent to incident.io endpoint.")
-			return true
+			defer res.Body.Close()
+			statusCode := res.StatusCode
+			if statusCode != 202 {
+				body, _ := ioutil.ReadAll(res.Body)
+				log.Println("Unable to notify incident.io endpoint:", string(body))
+				return false
+			} else {
+				log.Println("Notification sent to incident.io endpoint.")
+			}
 		}
 	}
-
+	return true
 }
